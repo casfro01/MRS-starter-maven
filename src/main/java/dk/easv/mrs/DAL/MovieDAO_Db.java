fdd8aa1bc60b6930705f24a217f1612e8c6b00ee
Casper <casfro01@easv365.dk>
2024-11-18 10:46:35 +0100

    gitignore config

---
+++ src/main/java/dk/easv/mrs/DAL/MovieDAO_Db.java
@@ -0,0 +1,31 @@
+package dk.easv.mrs.DAL;
+// project imports
+import dk.easv.mrs.BE.Movie;
+
+import java.util.List;
+
+// java imports
+
+public class MovieDAO_Db implements IMovieDataAccess {
+
+
+    @Override
+    public List<Movie> getAllMovies() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Movie createMovie(String title, int year) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateMovie(Movie movie) throws Exception {
+
+    }
+
+    @Override
+    public void deleteMovie(Movie movie) throws Exception {
+
+    }
+}
